34d2d13b56833bf3c54ac12d218b2c17b53e0bb4
arka <arkamallick30@gmail.com>
2022-10-29 02:47:32 +0200

    added waiter rest service in java

---
+++ backend/waiter/src/main/java/com/dr/sattlers/bar/employee/waiter/WaiterApplication.java
@@ -0,0 +1,13 @@
+package com.dr.sattlers.bar.employee.waiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiterApplication.class, args);
+	}
+
+}
