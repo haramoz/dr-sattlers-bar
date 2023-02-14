e75f3ee32088e70eef11b58233705b39ea29e001
Arka Mallick <arkamallick30@gmail.com>
2023-02-14 18:52:19 +0100

    adding junit

---
+++ backend/waiter/src/test/java/com/dr/sattlers/bar/employee/waiter/WaiterApplicationTest.java
@@ -0,0 +1,19 @@
+package com.dr.sattlers.bar.employee.waiter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WaiterApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before!");
+    }
+
+    @Test
+    void run() {
+        System.out.println("Successful!");
+    }
+}
